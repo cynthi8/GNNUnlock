9200f71907ae0d232be5141a85894a93786949a8
Erin Cold <coldegret@protonmail.com>
2021-03-10 01:59:54 -0500

    Setup tests
    Future development should be TDD. First test added to tease out the
    hamming block creator. Signature was captured from point_encrypt with
    SFLL-HD, k=8.
    
    pytest added to requirements.txt

---
+++ PythonEncrypt/tests/test_sfll_hd/test_sfll_hd.py
@@ -0,0 +1,13 @@
+import encrypt
+
+def test_ham_dist_block():
+    signature = (
+        f'  integer ham_dist, idx;\n'
+        f'  wire [7:0] diff;\n'
+        f'  assign diff = sat_res_inputs ^ keyvalue;\n\n'
+        f'  always@* begin\n'
+        f'    ham_dist = 0;\n'
+        f'    for(idx=0; idx<8; idx=idx+1) ham_dist = ham_dist + diff[idx];\n'
+        f'  end\n\n'
+    )
+    assert encrypt.ham_dist_block('sat_res_inputs', 'keyvalue', 8) == signature
