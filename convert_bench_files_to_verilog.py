f44ae16a272a3e4ea6eb9e51123fe0625217b13d
Erin Cold <coldegret@protonmail.com>
2021-03-19 17:32:48 -0400

    feat: Put Verilog converter output in correct subdirectory

--- convert_bench_files_to_verilog.py
+++ convert_bench_files_to_verilog.py
@@ -6,11 +6,17 @@ from pathlib import Path
 import read
 import write
 
-benchmark_directory = './Benchmarks'
-verilog_benchmark_directory = './VerilogBenchmarks'
+benchmark_dir = './Benchmarks'
+ISCAS_85_sub_dir = 'ISCAS-85'
+ITC_99_sub_dir = 'ITC-99'
 
-for walk_tuple in os.walk(benchmark_directory):
-    for file in glob(os.path.join(walk_tuple[0], '*.bench')):
+verilog_benchmark_dir = './VerilogBenchmarks'
+
+for sub_dir in [ISCAS_85_sub_dir, ITC_99_sub_dir]:
+    for file in glob(os.path.join(benchmark_dir, sub_dir, '*.bench')):
         graph = read.bench(file)
         print(file, graph.size())
-        write.verilog(graph, os.path.join(verilog_benchmark_directory, Path(file).stem))
+
+        dest_dir = os.path.join(verilog_benchmark_dir, sub_dir)
+        Path(dest_dir).mkdir(parents=True, exist_ok=True)
+        write.verilog(graph, os.path.join(dest_dir, Path(file).stem))
