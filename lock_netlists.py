3666b5f60cfad1d65f52ac924497c00e3aa3393a
Erin Cold <coldegret@protonmail.com>
2021-03-20 20:37:11 -0400

    feat: Fix and add SFLL locking to lock_netlists.py

--- lock_netlists.py
+++ lock_netlists.py
@@ -4,17 +4,72 @@ from pathlib import Path
 
 from encrypt import pointfunc
 
+verilog_benchmark_dir = 'VerilogBenchmarks'
+ISCAS_85_sub_dir = 'ISCAS-85'
+ITC_99_sub_dir = 'ITC-99'
+
+locked_netlists_path = 'LockedNetlists'
+anti_sat_sub_dir = 'AntiSAT'
+sfll_sat_sub_dir = 'SFLL'
+
+def file_count(dir):
+    return len([name for name in os.listdir(dir) if os.path.isfile(os.path.join(dir, name))])
+
+# NR -- Anti-SAT Generation
 key_sizes = [8, 16, 32, 64]
-h_values = [0, 2, 4]
-
-ISCAS_85_path = './VerilogBenchmarks/ISCAS-85'
-ITC_99_path = './VerilogBenchmarks/ITC-99'
-
-locked_netlists_path = './LockedNetlists'
-# NR -- Anti-SAT
-# FR -- SFLL-HD(2)
-for entry in os.scandir(ISCAS_85_path):
+lock_type = 'AntiSAT'
+dest_dir = os.path.join(locked_netlists_path, lock_type, ISCAS_85_sub_dir)
+Path(dest_dir).mkdir(parents=True, exist_ok=True)
+for entry in os.scandir(os.path.join(verilog_benchmark_dir, ISCAS_85_sub_dir)):
     if entry.path.endswith(".v") and entry.is_file():
+        circuit_name = Path(entry).stem
         for k in key_sizes:
-            pointfunc(entry.path,"NR", k, None, os.path.join(locked_netlists_path, f'{Path(entry).stem}_AntiSAT_{k}.v'))
-            pointfunc(entry.path,"FR", k, 2, os.path.join(locked_netlists_path, f'{Path(entry).stem}_SFLL-HD(2)_{k}.v'))
+            if (circuit_name == 'c3540' and k == 64):
+                continue # Skip due to limited PIs in circuit
+            for i in range(2):
+                pointfunc(entry.path,"NR", k, None, os.path.join(dest_dir, f'{circuit_name}_{lock_type}_{k}_{i}.v'))
+assert file_count(dest_dir) == 30
+
+key_sizes = [32, 64, 128]
+lock_type = 'AntiSAT'
+dest_dir = os.path.join(locked_netlists_path, lock_type, ITC_99_sub_dir)
+Path(dest_dir).mkdir(parents=True, exist_ok=True)
+for entry in os.scandir(os.path.join(verilog_benchmark_dir, ITC_99_sub_dir)):
+    if entry.path.endswith(".v") and entry.is_file():
+        circuit_name = Path(entry).stem
+        for k in key_sizes:
+            for i in range(2):
+                pointfunc(entry.path,"NR", k, None, os.path.join(dest_dir, f'{circuit_name}_{lock_type}_{k}_{i}.v'))
+assert file_count(dest_dir) == 36
+
+
+# FR -- SFLL-HD Generation
+key_sizes = [8, 16, 32, 64]
+h_values = [0, 2]
+for h in h_values:
+    lock_type = f'SFLL-HD({h})'
+    dest_dir = os.path.join(locked_netlists_path, lock_type, ISCAS_85_sub_dir)
+    Path(dest_dir).mkdir(parents=True, exist_ok=True)
+    for entry in os.scandir(os.path.join(verilog_benchmark_dir, ISCAS_85_sub_dir)):
+        if entry.path.endswith(".v") and entry.is_file():
+            circuit_name = Path(entry).stem
+            for k in key_sizes:
+                if (circuit_name == 'c3540' and k == 64):
+                    continue # Skip due to limited PIs in circuit
+                for i in range(3):
+                    pointfunc(entry.path,"FR", k, h, os.path.join(dest_dir, f'{circuit_name}_{lock_type}_{k}_{i}.v'))
+    assert file_count(dest_dir) == 45
+
+key_sizes = [32, 64, 128]
+h_values = [0, 2, 4]
+for h in h_values:
+    lock_type = f'SFLL-HD({h})'
+    dest_dir = os.path.join(locked_netlists_path, lock_type, ITC_99_sub_dir)
+    Path(dest_dir).mkdir(parents=True, exist_ok=True)
+    for entry in os.scandir(os.path.join(verilog_benchmark_dir, ITC_99_sub_dir)):
+        if entry.path.endswith(".v") and entry.is_file():
+            circuit_name = Path(entry).stem
+            for k in key_sizes:
+                for i in range(3):
+                    pointfunc(entry.path,"FR", k, h, os.path.join(dest_dir, f'{circuit_name}_{lock_type}_{k}_{i}.v'))
+    assert file_count(dest_dir) == 54
