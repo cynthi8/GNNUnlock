78346e81bd9d2948151f2229ec39f069b9c2189b
Erin Cold <coldegret@protonmail.com>
2021-03-24 05:41:46 -0400

    test: Use is_isomorphic() in test_write.py

--- PythonEncrypt/tests/test_write/test_write.py
+++ PythonEncrypt/tests/test_write/test_write.py
@@ -1,6 +1,7 @@
 import read
 import write
 import os
+import networkx as nx
 
 netlists = 'PythonEncrypt/tests/netlists'
 
@@ -8,16 +9,12 @@ def test_and_tree_bench():
     G_in = read.bench(os.path.join(netlists, 'and_tree.bench'))
     write.verilog(G_in,'temp')
     G = read.verilog('temp.v')
-    assert G.has_edge('N10', 'N14')
-    assert G.has_node('N9')
+    assert nx.is_isomorphic(G, G_in, lambda a, b: a['type'] == b['type'])
     os.remove('temp.v')
 
 def test_feed_through_bench():
     G_in = read.bench(os.path.join(netlists, 'feed_through.bench'))
     write.verilog(G_in,'temp')
     G = read.verilog('temp.v')
-    assert G.nodes['N3_I']['type'] == 'INPUT'
-    assert G.nodes['N3_O']['type'] == 'BUFF'
-    assert G.nodes['N3_O_OUT']['type'] == 'OUTPUT'
-    assert G.has_edge('N3_I', 'N3_O')
-    assert G.has_edge('N3_O', 'N3_O_OUT')
+    assert nx.is_isomorphic(G, G_in, lambda a, b: a['type'] == b['type'])
+    os.remove('temp.v')
