8cf9872cc3b992c802219f613dcdb1a6de06d936
Erin Cold <coldegret@protonmail.com>
2021-04-29 14:45:21 -0400

    Remove unused script

--- release_new_locked_netlists.sh
+++
@@ -1,5 +0,0 @@
-# After committing updates to master, checkout this branch and run this script to release a new set of locked netlists
-git merge -X theirs master
-./lock_netlists.py
-git commit -a -m "Release new locked netlists"
-git push
